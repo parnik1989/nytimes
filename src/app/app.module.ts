cadcedc2bafa3ea8b7e5457d7fef1cc43ba762e7
Parnik Gaurav <parnik1989@gmail.com>
2019-05-11 19:28:03 +0530

    First Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent,
-    NewsComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
